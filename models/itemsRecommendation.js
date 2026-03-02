09102edd84a2dd2858c35630d694ffcba2fcede0
Daniel <dk807@live.mdx.ac.uk>
2026-03-02 22:50:37 +0000

    change model

--- models/itemsRecommendation.js
+++ models/itemsRecommendation.js
@@ -4,48 +4,48 @@ const getDb = require('../util/database').getDb;
 
 class ItemRecommendation {
     static async findByID(resourceId) {
-  const db = getDb();
+      const db = getDb();
 
-  return db.collection('items-recommendation')
-    .aggregate([
-      {
-        $match: {
-          itemId: new mongodb.ObjectId(resourceId)
-        }
-      },
+      return db.collection('items-recommendation')
+        .aggregate([
+          {
+            $match: {
+              itemId: new mongodb.ObjectId(resourceId)
+            }
+          },
 
-      { $unwind: "$itemRecommendation" },
+          { $unwind: "$itemRecommendation" },
 
-      {
-        $lookup: {
-          from: "items-recommendation",
-          localField: "itemRecommendation.itemId",
-          foreignField: "itemId", // MUST match exact field
-          as: "recommendedDoc"
-        }
-      },
+          {
+            $lookup: {
+              from: "items-recommendation",
+              localField: "itemRecommendation.itemId",
+              foreignField: "itemId", // MUST match exact field
+              as: "recommendedDoc"
+            }
+          },
 
-      { $unwind: "$recommendedDoc" },
+          { $unwind: "$recommendedDoc" },
 
-      {
-        $sort: {
-          "itemRecommendation.score": -1,
-          "recommendedDoc.confidence": -1
-        }
-      },
+          {
+            $sort: {
+              "itemRecommendation.score": -1,
+              "recommendedDoc.confidence": -1
+            }
+          },
 
-      { $limit: 10 },
+          { $limit: 10 },
 
-      {
-        $project: {
-          _id: 0,
-          itemId: "$itemRecommendation.itemId",
-          score: "$itemRecommendation.score"
-        }
-      }
-    ])
-    .toArray();
-}
+          {
+            $project: {
+              _id: 0,
+              itemId: "$itemRecommendation.itemId",
+              score: "$itemRecommendation.score"
+            }
+          }
+        ])
+      .toArray();
+    }
 }
 
 module.exports = ItemRecommendation;
